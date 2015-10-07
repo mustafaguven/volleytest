1f3eab5e4033486349a39693f4131548a46d75e1
mustafaguven <mustafaguven@gmail.com>
2015-10-07 19:12:01 +0300

    calisan hali

--- app/src/main/java/mg/myapplication/MainActivity.java
+++ app/src/main/java/mg/myapplication/MainActivity.java
@@ -1,15 +1,20 @@
 package mg.myapplication;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -19,15 +24,19 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    enum EndPointType{LOGIN, CUSTOMER_INFO, BASKET_COUNT, LOGOUT}
+    enum EndPointType {LOGIN, CUSTOMER_INFO, BASKET_COUNT, LOGOUT}
+    private static final String SET_COOKIE_KEY = "Set-Cookie";
+    private static final String COOKIE_KEY = "Cookie";
+    private static final String SESSION_COOKIE = "DnR.customer";
+    private SharedPreferences _preferences;
     String sessionId = "";
+    Map<String, String> mHeaders = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        _preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -73,81 +83,120 @@ public class MainActivity extends AppCompatActivity {
         sendRequest(EndPointType.LOGIN, url, body);
     }
 
-    private void sendRequest(final EndPointType type, String url, String body) {
+    private void sendRequest(final EndPointType type, final String url, String body) {
 
 
-        JsonObjectRequest jsonRequest = new JsonObjectRequest
-                (Request.Method.POST, url, body, new Response.Listener<JSONObject>() {
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, body, new Response.Listener<JSONObject>() {
 
 
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        // the response is already constructed as a JSONObject!
-                        switch (type){
-                            case LOGIN:
-                                try {
-                                    sessionId = response.getString("SessionObject");
-                                    getCustomerInfo();
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                                break;
-                            case CUSTOMER_INFO:
-                                getBasketCount();
-                                break;
-                            case BASKET_COUNT:
-                                try {
-                                    ((TextView)(findViewById(R.id.lblBasketCount))).setText(response.getString("CartItemCount"));
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                                break;
-                            case LOGOUT:
-                                break;
-                            default:
-                                break;
+            @Override
+            public void onResponse(JSONObject response) {
+                // the response is already constructed as a JSONObject!
+                try {
+                    Log.e("SENDREQUEST", type + " " + response.getString("success") + " " + url);
+                } catch (JSONException e) {
+                    Log.e("SENDREQUEST", type + " " + e.getMessage() + " " + url);
+                }
+                switch (type) {
+                    case LOGIN:
+                        try {
+                            sessionId = response.getString("SessionObject");
+                            getCustomerInfo();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
+                        break;
+                    case CUSTOMER_INFO:
+                        getBasketCount();
+                        break;
+                    case BASKET_COUNT:
+                        try {
+                            ((TextView) (findViewById(R.id.lblBasketCount))).setText(response.getString("CartItemCount"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case LOGOUT:
+                        break;
+                    default:
+                        break;
+                }
 
-                    }
-                }, new Response.ErrorListener() {
 
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        error.printStackTrace();
-                    }
+            }
+        }, new Response.ErrorListener() {
 
-                });
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
 
+        }) {
+
+            @Override
+            protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+                // since we don't know which of the two underlying network vehicles
+                // will Volley use, we have to handle and store session cookies manually
+                checkSessionCookie(response.headers);
+                //mHeaders = response.headers;
+                return super.parseNetworkResponse(response);
+            }
+
+            /* (non-Javadoc)
+             * @see com.android.volley.Request#getHeaders()
+             */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = super.getHeaders();
+
+                if (headers == null
+                        || headers.equals(Collections.emptyMap())) {
+                    headers = new HashMap<>();
+                }
+
+                //headers.put("Accept", "application/json");
+                //headers.put("Set-Cookie", "DnR.customer=173fb43c-ab20-422a-b45c-53229219c923; expires=Thu, 06-Oct-2016 14:57:12 GMT; path=/; HttpOnly");
+                //headers.put("Content-Type", "application/json; charset=utf-8");
+                //headers.put("Accept-Language", "en-US,en;q=0.8,en-GB;q=0.6,tr;q=0.4,ko;q=0.2,ro;q=0.2");
+                addSessionCookie(headers);
+/*                if(mHeaders!=null)
+                    headers = mHeaders;*/
+
+                return headers;
+            }
+        }
+
+
+
+/*        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("Content-Type", "application/json");
+                params.put("Accept", "application/json");
+
+                return params;
+            }
+        }*/
+
+                ;
 
 
         Volley.newRequestQueue(this).add(jsonRequest);
     }
 
-    private Map<String, String> addHeaders() {
-
-        Map<String, String> headers = new HashMap<>();
-        headers.put("User-Agent", System.getProperty("http.agent"));
-        headers.put("Content-Type", "application/json");
-        headers.put("Accept", "application/json");
-        return headers;
-    }
 
 
     private void getBasketCount() {
         String url = "http://www.dr.com.tr/ApiCustomer/GetShoppingCartItemCount/?signedRequest=" + sessionId;
-        try {
-            url = URLEncoder.encode(url, "ISO-8859-1");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        String body = "";
+        String body = "{}";
+
         sendRequest(EndPointType.BASKET_COUNT, url, body);
     }
 
     private void getCustomerInfo() {
-        String url = "http://www.dr.com.tr/ApiCustomer/Info/?signedRequest=" + sessionId;
-        String body = "";
+        String url = "http://www.dr.com.tr/ApiCustomer/info/?signedRequest=" + sessionId;
+        String body = "{}";
         sendRequest(EndPointType.CUSTOMER_INFO, url, body);
     }
 
@@ -171,5 +220,42 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public final void checkSessionCookie(Map<String, String> headers) {
+        if (headers.containsKey(SET_COOKIE_KEY)
+                && headers.get(SET_COOKIE_KEY).startsWith(SESSION_COOKIE)
+
+                ) {
+            String cookie = headers.get(SET_COOKIE_KEY);
+            if (cookie.length() > 0) {
+                String[] splitCookie = cookie.split(";");
+                String[] splitSessionId = splitCookie[0].split("=");
+                cookie = splitSessionId[1];
+                SharedPreferences.Editor prefEditor = _preferences.edit();
+                prefEditor.putString(SESSION_COOKIE, cookie);
+                prefEditor.commit();
+            }
+        }
+    }
+
+    /**
+     * Adds session cookie to headers if exists.
+     * @param headers
+     */
+    public final void addSessionCookie(Map<String, String> headers) {
+        String sessionId = _preferences.getString(SESSION_COOKIE, "");
+        if (sessionId.length() > 0) {
+            StringBuilder builder = new StringBuilder();
+            builder.append(SESSION_COOKIE);
+            builder.append("=");
+            builder.append(sessionId);
+            if (headers.containsKey(COOKIE_KEY)) {
+                builder.append("; ");
+                builder.append(headers.get(COOKIE_KEY));
+            }
+            headers.put(COOKIE_KEY, "DnR.AUTH=89F62881F3911DB32EA3C8F5697E56A7D1BCEEE2337AB8B6C209ECED407E62A4E48787DBB66B62BABF4696A261C2A1E3980101AFF64DEE4FBE7125220C451D8D67BAB631274E73809852F05A9EEA2780385C4ED3CE16D49C99856ED80051AD1A9500432EC92903F5D3EC388ED739E269D1B2E82F0CAC4909FFBD46F41E37A502C1F4CAD0021E48235F181864E5E644334183DAA5AD03D29980DDB100E8564F50;  ");
+        }
     }
 }
